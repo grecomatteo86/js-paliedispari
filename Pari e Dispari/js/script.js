f52cbce31a147be9fd0769bb03711efe62e59396
Matteo <grecomatteo86@gmail.com>
2021-07-26 13:11:18 +0200

    Pari e dispari: validation one

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,17 +1,51 @@
-/*
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-Sommiamo i due numeri
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-Dichiariamo chi ha vinto.
-*/
+var pariDispari = prompt('choose: even or odd');
+while (pariDispari != 'even' && pariDispari != 'odd') {
+  alert("Please enter EVEN or ODD");
+  pariDispari = prompt('choose: even or odd');
+}
+console.log('bravo');
 
-//1. imposto una funzione che genera un numero random
-function numeroRandom () {
+
+
+
+
+// if (pariDispari == 'pari') {
+//   pariDispari = true;
+//   alert('hai scelto pari, allora in computer sceglierà dispari');
+// } else {
+//   pariDispari = false;
+//   alert('hai scelto dispari, allora in computer sceglierà pari');
+// }
+//
+// //4. l'utente inserisce un numero da 1 a 5 e invoco la funzione getRandomNumber per generare un numero random per il computer
+// var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 5'));
+// alert('il numero da te scelto è ' + numeroUtente);
+//
+// var numeroCpu = getRandomNumber ();
+// alert('il numero scelto dal computer è ' + numeroCpu);
+//
+//
+// //5. invoco la funzione sommaNumeri per sommare numeroUtente e numeroCpu. se pari allora 'totale' è true, altrimenti è false
+// if (sumIsEven(numeroUtente,numeroCpu)) {
+//   alert("la somma dei due numeri è pari!");
+//   var totale = true;
+// } else {
+//   alert("la somma dei due numeri è dispari!");
+//   var totale = false;
+// }
+//
+// // 6. comparo la scelta dell'utente e la variabile 'totale': se hanno lo stesso valore, l'utente vince, altrimenti perde
+// if ( pariDispari == totale ) {
+//   alert('hai vinto');
+// } else {
+//   alert('hai perso');
+// }
+
+// Function - getRandomNumber
+function getRandomNumber () {
   return Math.floor(Math.random() * 6);
 }
-
-//2. imposto una funzione che mi dice se la somma tra due numeri è pari
+// Function - sumIsEven
 function sumIsEven (num1, num2) {
   var somma = parseInt(num1 + num2);
   console.log('la somma dei due numeri è ' + somma);
@@ -20,39 +54,4 @@ function sumIsEven (num1, num2) {
   } else {
     return false;
   }
-}
-
-//3. l'utente sceglie tra pari o dispari. se 'pari' la var pariDispari è vera, altrimenti è falsa
-var pariDispari = prompt('scegli: pari o dispari');
-
-if (pariDispari == 'pari') {
-  pariDispari = true;
-  alert('hai scelto pari, allora in computer sceglierà dispari');
-} else {
-  pariDispari = false;
-  alert('hai scelto dispari, allora in computer sceglierà pari');
-}
-
-//4. l'utente inserisce un numero da 1 a 5 e invoco la funzione numeroRandom per generare un numero random per il computer
-var numeroUtente = parseInt(prompt('inserisci un numero da 1 a 5'));
-alert('il numero da te scelto è ' + numeroUtente);
-
-var numeroCpu = numeroRandom ();
-alert('il numero scelto dal computer è ' + numeroCpu);
-
-
-//5. invoco la funzione sommaNumeri per sommare numeroUtente e numeroCpu. se pari allora 'totale' è true, altrimenti è false
-if (sumIsEven(numeroUtente,numeroCpu)) {
-  alert("la somma dei due numeri è pari!");
-  var totale = true;
-} else {
-  alert("la somma dei due numeri è dispari!");
-  var totale = false;
-}
-
-// 6. comparo la scelta dell'utente e la variabile 'totale': se hanno lo stesso valore, l'utente vince, altrimenti perde
-if ( pariDispari == totale ) {
-  alert('hai vinto');
-} else {
-  alert('hai perso');
 }
